5368f3d7100ba319daf17b5525e056ac4bd7b5de
Mathias Pius <contact@pius.io>
2023-06-28 12:42:52 +0200

    Initial commit

---
+++ dnsetes-crds/src/lib.rs
@@ -0,0 +1,26 @@
+use kube::CustomResource;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
+#[kube(
+    group = "dnsetes.pius.dev",
+    version = "v1alpha1",
+    kind = "DNSZone",
+    namespaced
+)]
+//#[kube(status = "BlockStatus")]
+//#[kube(printcolumn = r#"{"name":"size", "jsonPath": ".spec.size", "type": "string"}"#)]
+//#[kube(printcolumn = r#"{"name":"claim", "jsonPath": ".spec.claim.name", "type": "string"}"#)]
+//#[kube(printcolumn = r#"{"name":"port", "jsonPath": ".status.portNumber", "type": "integer"}"#)]
+#[kube(printcolumn = r#"{"name":"zone", "jsonPath": ".spec.name", "type": "string"}"#)]
+#[kube(printcolumn = r#"{"name":"serial", "jsonPath": ".spec.serial", "type": "integer"}"#)]
+#[serde(rename_all = "camelCase")]
+pub struct DNSZoneSpec {
+    pub name: String,
+    pub serial: Option<u32>,
+    pub refresh: Option<u32>,
+    pub retry: Option<u32>,
+    pub expire: Option<u32>,
+    pub negative_response_cache: Option<u32>
+}
