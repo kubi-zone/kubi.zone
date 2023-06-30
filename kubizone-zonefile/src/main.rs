8b1545f6bff910be484bdf1e32a4a06ab7963516
Mathias Pius <contact@pius.io>
2023-06-30 09:40:44 +0200

    Move crd dumping capabilities into the respective controllers and change crd-dumper into a library providing utilities for doing so

--- kubizone-zonefile/src/main.rs
+++ kubizone-zonefile/src/main.rs
@@ -1,26 +1,55 @@
+use std::path::PathBuf;
+
+use clap::{command, Parser, Subcommand};
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
-use kube::{
-    api::{DeleteParams, PostParams},
-    Api, Client, CustomResourceExt,
-};
+use kube::{Api, Client};
 use kubizone_zonefile_crds::ZoneFile;
+use tracing::log::*;
 
 mod reconciliation;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    PrintCrds,
+    DumpCrds { path: PathBuf },
+    Reconcile { danger_recreate_crds: bool },
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let client = Client::try_default().await.unwrap();
 
-    let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
+    let args = Args::parse();
 
-    crds.delete(ZoneFile::crd_name(), &DeleteParams::default())
-        .await
-        .ok();
+    match args.command {
+        Command::PrintCrds => {
+            println!(
+                "{}",
+                kubizone_crd_utils::serialize_crd::<ZoneFile>().unwrap()
+            );
+        }
+        Command::DumpCrds { path } => {
+            kubizone_crd_utils::write_to_path::<ZoneFile>(&path).unwrap();
+        }
+        Command::Reconcile {
+            danger_recreate_crds,
+        } => {
+            let client = Client::try_default().await.unwrap();
 
-    crds.create(&PostParams::default(), &ZoneFile::crd())
-        .await
-        .unwrap();
+            if danger_recreate_crds {
+                warn!("flag --danger-recreate-crds set, deleting ZoneFile CRDs from cluster, and recreating. This will delete all existing ZoneFiles!");
+                let api: Api<CustomResourceDefinition> = Api::all(client.clone());
+                kubizone_crd_utils::recreate_crd_destructively::<ZoneFile>(api).await;
+            }
 
-    reconciliation::reconcile(client).await;
+            reconciliation::reconcile(client).await;
+        }
+    }
 }
--- kubizone/src/main.rs
+++ kubizone/src/main.rs
@@ -1,15 +1,60 @@
+use std::path::PathBuf;
+
+use clap::{command, Parser, Subcommand};
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
 use kube::{
     api::{DeleteParams, PostParams},
     Api, Client, CustomResourceExt,
 };
 use kubizone_crds::{Record, Zone};
+use tracing::log::warn;
 
 mod reconciliation;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    PrintCrds,
+    DumpCrds { path: PathBuf },
+    Reconcile { danger_recreate_crds: bool },
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    match args.command {
+        Command::PrintCrds => {
+            println!("{}", kubizone_crd_utils::serialize_crd::<Zone>().unwrap());
+            println!("{}", kubizone_crd_utils::serialize_crd::<Record>().unwrap());
+        }
+        Command::DumpCrds { path } => {
+            kubizone_crd_utils::write_to_path::<Zone>(&path).unwrap();
+            kubizone_crd_utils::write_to_path::<Record>(&path).unwrap();
+        }
+        Command::Reconcile {
+            danger_recreate_crds,
+        } => {
+            let client = Client::try_default().await.unwrap();
+
+            if danger_recreate_crds {
+                warn!("flag --danger-recreate-crds set, deleting Zone and Record CRDs from cluster, and recreating. This will delete all existing Records and Zones!");
+                let api: Api<CustomResourceDefinition> = Api::all(client.clone());
+                kubizone_crd_utils::recreate_crd_destructively::<Zone>(api.clone()).await;
+                kubizone_crd_utils::recreate_crd_destructively::<Record>(api.clone()).await;
+            }
+
+            reconciliation::reconcile(client).await;
+        }
+    }
+
     let client = Client::try_default().await.unwrap();
 
     let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
